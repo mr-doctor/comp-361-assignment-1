674ec5eacfa0881616b3bcbbad5c29077b136c7b
daniel <daniel@gilbertpinfold.co.nz>
2018-08-09 21:48:37 +1200

    Complete algorithm

--- src/Tromino.java
+++ src/Tromino.java
@@ -1,81 +1,156 @@
 public class Tromino {
 
-	// deficient parts of the board are represented by -1, unfilled portions by 0, filled by 1
-	private int[][] board;
+	// deficient parts of the board are represented by the X character, unfilled portions by a space,
+	// 		filled by a character of the alphabet (one per tromino)
+	private char[][] board;
+	private char trominoIndex = 'a';
+
+	int step = 0;
+
+	private static final char EMPTY_TILE = ' ';
+	private static final char DEFICIENT_TILE = 'X';
 
 	public Tromino(int size, int x, int y) {
-
-		board = new int[size][size];
+		board = new char[size][size];
 
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				board[i][j] = 0;
+				board[i][j] = EMPTY_TILE;
 			}
 		}
 
-		board[y][x] = -1;
+		board[x][y] = DEFICIENT_TILE;
 	}
 
-	public void tile(int size, int xStart, int yStart) {
+	private void tile(int size, int xStart, int yStart) {
+
 		// only one tromino will fit on this board
 		if (size == 2) {
 			// check every tile
 			for (int i = 0; i < size; i++) {
 				for (int j = 0; j < size; j++) {
-					// if the tile is not deficient
-					if (board[xStart + i][yStart + j] >= 0) {
+					// if the tile is not deficient and unfilled
+					if (board[xStart + i][yStart + j] == EMPTY_TILE) {
 						// fill in the board
-						board[xStart + i][yStart + j] = 1;
+						board[xStart + i][yStart + j] = trominoIndex;
 					}
 				}
 			}
+			// move onto the next character to fill in
+			trominoIndex++;
 		} else {
 
 			int xDeficient = xStart;
 			int yDeficient = yStart;
 
+			// find the deficient tile
 			for (int x = xStart; x < xStart + size; x++) {
 				for (int y = yStart; y < yStart + size; y++) {
-					if (board[x][y] < 0) {
+					if (board[x][y] != EMPTY_TILE) {
 						xDeficient = x;
 						yDeficient = y;
 					}
 				}
 			}
 
+			int newSize = size / 2;
+
 			// check if deficient tile is in top left quad
-			if (xDeficient < xStart + size / 2 && yDeficient < yStart + size / 2) {
+			if (xDeficient < xStart + newSize && yDeficient < yStart + newSize) {
 
-				tile(size / 2, xStart, yStart);
+				tile(newSize, xStart, yStart); // tile(n/2, m1)
 
-				board[xStart + size / 2][yStart + size / 2] = 1;
-				board[xStart + size / 2][yStart + size / 2 - 1] = 1;
-				board[xStart + size / 2 - 1][yStart + size / 2] = 1;
+				board[xStart + newSize][yStart + newSize - 1] = trominoIndex;
+				board[xStart + newSize][yStart + newSize] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize] = trominoIndex;
+
+				trominoIndex++;
 
 				// recurse into bottom left
-				tile(size / 2, xStart, yStart + size / 2);
+				tile(newSize, xStart, yStart + newSize); // tile(n/2, m2)
+				// recurse into top right
+				tile(newSize, xStart + newSize, yStart); // tile(n/2, m3)
 				// recurse into bottom right
-				tile(size / 2, xStart + size / 2, yStart);
+				tile(newSize, xStart + newSize, yStart + newSize); // tile(n/2, m4)
+
+				// check if deficient tile is in bottom left quad
+			} else if (xDeficient < xStart + newSize && yDeficient >= yStart + newSize) {
+
+				tile(newSize, xStart, yStart + newSize);
+
+				board[xStart + newSize][yStart + newSize - 1] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize - 1] = trominoIndex;
+				board[xStart + newSize][yStart + newSize] = trominoIndex;
+
+				trominoIndex++;
+
+				// recurse into top left
+				tile(newSize, xStart, yStart); // tile(n/2, m2)
+				// recurse into top right
+				tile(newSize, xStart + newSize, yStart); // tile(n/2, m3)
+				// recurse into bottom right
+				tile(newSize, xStart + newSize, yStart + newSize); // tile(n/2, m4)
+
+				// check if deficient tile is in top right quad
+			} else if (xDeficient >= xStart + newSize && yDeficient < yStart + newSize) {
+
+				tile(newSize, xStart + newSize, yStart); // tile(n/2, m1)
+
+				board[xStart + newSize][yStart + newSize] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize - 1] = trominoIndex;
+
+				trominoIndex++;
+
+				// recurse into top left
+				tile(newSize, xStart, yStart); // tile(n/2, m2)
+				// recurse into bottom left
+				tile(newSize, xStart, yStart + newSize); // tile(n/2, m3)
+				// recurse into bottom right
+				tile(newSize, xStart + newSize, yStart + newSize); // tile(n/2, m4)
+
+				// bottom right
+			} else if (xDeficient >= xStart + newSize && yDeficient >= yStart + newSize) {
+
+				tile(newSize, xStart + newSize, yStart + newSize);
+
+				board[xStart + newSize][yStart + newSize - 1] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize - 1] = trominoIndex;
+				board[xStart + newSize - 1][yStart + newSize] = trominoIndex;
+
+				trominoIndex++;
+
+				// recurse into top right
+				tile(newSize, xStart + newSize, yStart); // tile(n/2, m3)
+				// recurse into bottom left
+				tile(newSize, xStart, yStart + newSize); // tile(n/2, m4)
+				// recurse into top left
+				tile(newSize, xStart, yStart); // tile(n/2, m2)
+
+				// check if deficient tile is in top right quad
 			}
 		}
-		print();
-		System.out.println();
-
+		System.out.println(step++);
+		System.out.println(this + "\n");
 	}
 
-	public void print() {
-
-		for (int i=0; i<board.length; i++) {
-			for (int j=0; j<board[i].length; j++)
-				System.out.print(board[i][j] + "\t");
-			System.out.println();
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (char[] aBoard : board) {
+			for (char anABoard : aBoard) {
+				stringBuilder.append("[").append(anABoard).append("]");
+			}
+			stringBuilder.append("\n");
 		}
+		return stringBuilder.toString();
 	}
 
 	public static void main(String[] args) {
-		Tromino t = new Tromino(4, 1, 1);
-		t.tile(4, 0, 0);
+		int size = 8;
 
-		t.print();
+		Tromino t = new Tromino(size, 1, 1);
+		t.tile(size, 0, 0);
+
 	}
 }
